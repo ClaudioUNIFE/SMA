b5e1f3e7c2666e60be3143a08e6112940ce3c84d
3manuel3 <emanuelepio.dicanio@edu.unife.it>
2024-05-14 14:31:20 +0200

    ciao

--- app/Models/Xit.php
+++ app/Models/Xit.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 use Laravel\Sanctum\HasApiTokens;
 use App\Models\Find;
 
