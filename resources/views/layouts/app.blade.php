365d15589e6cdb490abb81db424d3bb1496b1083
PalellaPiergaetano <piergaetanop.palella@edu.unife.it>
2024-06-25 15:58:36 +0200

    ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head style="background-color: rgb(18, 24, 38)">
+    <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -14,7 +14,7 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased" style="background-color: rgb(18, 24, 38)">
+    <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @include('layouts.navigation')
 
@@ -34,7 +34,7 @@
         </div>
 
     </body>
-    <footer style="background-color: rgb(18, 24, 38)">
+    <footer>
     <br>
     <br>
     <br>
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,5 +17,7 @@ export default {
         },
     },
 
+    darkMode: false, // Disables dark mode
+
     plugins: [forms],
 };
