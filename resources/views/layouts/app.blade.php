9895a941daef25784f264830c5f3e3512f436f67
PalellaPiergaetano <piergaetanop.palella@edu.unife.it>
2024-06-25 15:52:21 +0200

    ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
+    <head style="background-color: rgb(18, 24, 38)">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -14,7 +14,7 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased" style="background-color: rgb(18, 24, 38)">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @include('layouts.navigation')
 
@@ -32,5 +32,12 @@
                 {{ $slot }}
             </main>
         </div>
+
     </body>
+    <footer style="background-color: rgb(18, 24, 38)">
+    <br>
+    <br>
+    <br>
+    <br>
+    </footer>
 </html>
